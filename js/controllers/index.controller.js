915f72913c9ee88312b9ce1edf079edca472773c
Rob Romaniw <robertromaniw@gmail.com>
2016-12-04 12:29:33 -0500

    search input is console.logged

--- js/controllers/index.controller.js
+++ js/controllers/index.controller.js
@@ -10,7 +10,12 @@
 
   function IndexController(TweetFactory){
     this.newTweet = new TweetFactory();
-    
+    this.addSearch = function () {
+      console.log('clicked')
+      let search = {body: this.newSearch}
+      console.log(search.body)
+    }
+
   }
 
 }());
