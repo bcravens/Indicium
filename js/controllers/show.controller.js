ad787b370a1f01219f6ad71253eb8e9559ad9d23
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-12-05 15:57:48 -0500

    searches now posting to db

--- js/controllers/show.controller.js
+++ js/controllers/show.controller.js
@@ -16,8 +16,7 @@
     this.response.$promise.then((data) => {
       this.sentiment = data.sentiment;
       this.tweets = data.tweets
-      console.log(this.tweets)
-      console.log(this.sentiment)
+  
     })
   }
 
