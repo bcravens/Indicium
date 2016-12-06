e7e9ddea6c2bd6ecee11031a4a6626afc66d827d
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-12-06 17:15:27 -0500

    connects front end and backend

--- js/controllers/show.controller.js
+++ js/controllers/show.controller.js
@@ -11,13 +11,16 @@
 
   function ShowController($stateParams, TweetFactory){
 
-    // this.response = TweetFactory.get({search: $stateParams.search});
-    //
-    // this.response.$promise.then((data) => {
-    //   this.sentiment = data.sentiment;
-    //   this.tweets = data.tweets
-    //
-    // })
+    this.response = TweetFactory.get({search: $stateParams.search});
+    this.response.$promise.then((data) => {
+      this.tweets = data.tweets
+      this.sentiment = data.watsonResponse;
+      this.anger = Math.round((parseFloat(this.sentiment.docEmotions.anger) * 100)) + "%"
+      this.disgust = Math.round((parseFloat(this.sentiment.docEmotions.disgust) * 100)) + "%"
+      this.fear = Math.round((parseFloat(this.sentiment.docEmotions.fear) * 100)) + "%"
+      this.joy = Math.round((parseFloat(this.sentiment.docEmotions.joy) * 100)) + "%"
+      this.sadness = Math.round((parseFloat(this.sentiment.docEmotions.sadness) * 100)) + "%"
+    })
   }
 
 }());
