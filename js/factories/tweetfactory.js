7b71b0837f54ba39729159e1a891d4e4451b030b
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-12-08 14:24:00 -0500

    reverts back to non-heroku setup

--- js/factories/tweetfactory.js
+++ js/factories/tweetfactory.js
@@ -9,7 +9,7 @@
   ]);
 
   function TweetFactoryFunction($resource){
-  return $resource("https://pure-plains-26399.herokuapp.com/api/tweets/:search", {}, {
+  return $resource("http://localhost:4000/api/tweets/:search", {}, {
       update: {method: "PUT"}
     });
   }
