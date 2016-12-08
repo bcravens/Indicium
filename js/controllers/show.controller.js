987f5521a05e479721c5c5f47861d9c20fa0d1df
Bradley Cravens <bradleycravens@gmail.com>
2016-12-08 10:40:00 -0500

    Added links to associations

--- js/controllers/show.controller.js
+++ js/controllers/show.controller.js
@@ -10,11 +10,11 @@
   ]);
 
   function ShowController($stateParams, TweetFactory){
-    this.searchTerm = $stateParams.search.toUpperCase()
+    this.searchTerm = $stateParams.search.toUpperCase();
 
     this.response = TweetFactory.get({search: $stateParams.search});
     this.response.$promise.then((data) => {
-      this.tweets = data.tweets
+      this.tweets = data.tweets;
       this.sentiment = data.watsonResponse;
       this.anger = Math.round((parseFloat(this.sentiment.docEmotions.anger) * 100)) + "%"
       this.disgust = Math.round((parseFloat(this.sentiment.docEmotions.disgust) * 100)) + "%"
@@ -28,6 +28,11 @@
       this.joyNum = parseInt(this.joy)
       this.sadnessNum = parseInt(this.sadness)
     })
+
+    this.roundSentiment = function(sentiment){
+      var value = Math.round(sentiment * 100);
+      return value
+    }
 
   }
 
